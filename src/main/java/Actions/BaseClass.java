3364f53f9512bc08100cc9efa4cbffbb6d808e3d
Kingsly Bose <kingsly.bose@experian.com>
2019-12-16 21:18:00 +0000

    Second Commit

--- src/main/java/Actions/BaseClass.java
+++ src/main/java/Actions/BaseClass.java
@@ -1,19 +1,28 @@
 package Actions;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Pages.LoginPage;
 
 public class BaseClass {
 
     public WebDriver driver;
+    
     @BeforeTest
     public void launchBrowser(){
-        System.out.println("kjkjkjkjkjk");
+
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\C53180A\\Desktop\\driver\\chromedriver_win32\\chromedriver.exe");
 
-        WebDriver driver=new ChromeDriver();
+        driver=new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https:www.google.co.in");
+        
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://demo.guru99.com/V4/");
+        
     }
 }
--- src/main/java/Actions/LoginTest.java
+++ src/main/java/Actions/LoginTest.java
@@ -1,11 +1,34 @@
 package Actions;
 
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginTest {
+import Pages.HomePage;
+import Pages.LoginPage;
 
-    //@Test
-    public void loginTest(){
-        System.out.println("Test");
-    }
+public class LoginTest extends BaseClass{
+
+	LoginPage objLogin;
+	HomePage objHomePage;
+
+	@Test(priority = 0)
+	public void test_Home_Page_Appear_Correct() {
+		
+		objLogin = new LoginPage(driver);
+
+		// Verify login page title
+		String loginPageTitle = objLogin.getLoginTitle();
+		Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
+		
+		// login to application
+		objLogin.loginToGuru99("mgr123", "mgr!23");
+		
+		// go the next page
+		objHomePage = new HomePage(driver);
+		
+		// Verify home page
+		Assert.assertTrue(objHomePage.getHomePageDashboardUserName().toLowerCase().contains("manger id : mgr123"));
+	}
+
 }
